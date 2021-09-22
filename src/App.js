372c25efe6df52ce910c2c732e75258cfc0be61d
aaaasif <alexasif4@gmail.com>
2021-09-22 17:42:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+function Countries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+
+  }, []);
+
+  return (
+    <div>
+      <h2>Traveling around the world!!</h2>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Capital: {props.capital}</p>
+    </div>
+  )
+} */
+
+export default App;
