ae8db2b2bbe3ba6b7730b17978d015bda836d8fe
aaaasif <alexasif4@gmail.com>
2021-09-23 11:09:53 +0600

    js added

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>hello from react</h1>
+        </div>
+    );
+};
+
+export default Countries;c
